6f89eda3c0799c688a5551f4328764453dff67e2
obebe <shkim000905@naver.com>
2024-01-02 02:18:35 +0900

    mvc 파일 생성

---
+++ lib/view/login/login_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginMain extends StatelessWidget {
+  const LoginMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
