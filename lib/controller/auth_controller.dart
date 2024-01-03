b5d0b8946515009ef34089e0aa4a9a7427085e91
obebe <shkim000905@naver.com>
2024-01-04 03:31:52 +0900

    method 구현 중

--- lib/controller/auth_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/get.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-class AuthController extends GetxController {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-}
