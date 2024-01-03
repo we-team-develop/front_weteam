5a070606655a67cac10e65b931eb6fb1ba275fe6
obebe <shkim000905@naver.com>
2024-01-04 03:16:19 +0900

    feat:logout_kakao

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthController extends GetxController {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+}
