a8029a37a761a9ac850ae6c2169a7125a2f6c367
Jungwuk <vojougae35@gmail.com>
2024-01-05 03:49:52 +0900

    'isTeamListEmpty'을 true로 설정합니다

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -15,7 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool hasFixedDDay = false;
   int currentSliderIndex = 0;
-  bool isTeamListEmpty = false; // TODO: 팀플 비어있는지 확인하기, Controller 만들기
+  bool isTeamListEmpty = true; // TODO: 팀플 비어있는지 확인하기, Controller 만들기
 
   @override
   Widget build(BuildContext context) {
