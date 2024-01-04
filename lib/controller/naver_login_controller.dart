96004da6c5b694ee632068e71bedb6a76a3a0b11
Jungwuk <vojougae35@gmail.com>
2024-01-04 14:00:12 +0900

    네이버 로그인에 Firebase Custom Token 사용

--- lib/controller/naver_login_controller.dart
+++ lib/controller/naver_login_controller.dart
@@ -1,15 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:front_weteam/controller/auth_controller.dart';
+import 'package:front_weteam/social/create_firebase_custom_token.dart';
 
 class NaverLoginController extends AuthController {
   @override
   Future<String> getToken() async {
     if (!(await isLoggedIn())) {
-      // TODO: 로그인 상태가 아닐 경우 핸들링
-      throw UnimplementedError();
+      debugPrint("NaverLoginController: getToken이 호출되었지만 로그인 상태가 아님");
+      return "";
     }
 
-    // TODO: firebase custom token?
+    return await FirebaseAuth.instance.currentUser?.getIdToken() ?? "";
+  }
+
+  Future<String> getAccessToken() async {
     final NaverAccessToken res = await FlutterNaverLogin.currentAccessToken;
     return res.accessToken;
   }
@@ -22,11 +28,25 @@ class NaverLoginController extends AuthController {
   @override
   Future<bool> login() async {
     try {
-      final NaverLoginResult res = await FlutterNaverLogin.logIn();
+      await FlutterNaverLogin.logIn();
       bool success = await isLoggedIn(); // 성공 여부
-      return success;
+
+      if (!success) {
+        return false;
+      }
+
+      String accessToken = await getAccessToken(); // 네이버 액세스 토큰을 받아옴
+
+      // FirebaseToken 생성 시작
+      final customToken = await createFirebaseCustomToken(
+        accessToken: accessToken,
+        loginType: 'naverCustomAuth', // 네이버 커스텀 auth 함수 사용
+      );
+      final userCredential = await FirebaseAuth.instance
+          .signInWithCustomToken(customToken); // fb 로그인
+      return userCredential.user != null;
     } catch (error) {
-      // TODO: 로그인 오류 핸들링
+      debugPrint(error.toString());
       return false;
     }
   }
@@ -37,7 +57,6 @@ class NaverLoginController extends AuthController {
       await FlutterNaverLogin.logOut();
       return true;
     } catch (error) {
-      // TODO: 로그아웃 오류 핸들링
       return false;
     }
   }
