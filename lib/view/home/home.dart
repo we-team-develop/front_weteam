1292cda32feaed5ac947b751a849515ff4eae426
Jungwuk <vojougae35@gmail.com>
2024-01-05 19:30:34 +0900

    feat: 팀플 추가 dialog 기초 구현 1

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -187,10 +187,15 @@ class _HomeState extends State<Home> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(
-                    ImagePath.icPlus,
-                    height: 34,
-                    width: 34,
+                  GestureDetector(
+                    onTap: () {
+                      addTeamDialog();
+                    },
+                    child: Image.asset(
+                      ImagePath.icPlus,
+                      height: 34,
+                      width: 34,
+                    ),
                   ),
                   const SizedBox(
                     height: 15,
@@ -424,5 +429,165 @@ class _HomeState extends State<Home> {
                 ],
               ),
             )));
+  }
+
+  void addTeamDialog() {
+    showDialog(
+        context: context,
+        //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+        //barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: const Color(0xFFFFFFFF),
+            surfaceTintColor: const Color(0xFFFFFFFF),
+            // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            //Dialog Main Title
+            title: const Text(
+              '팀플 추가',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFF333333),
+                fontSize: 12,
+                fontFamily: 'NanumSquareNeo',
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            //
+            content: Column(
+              mainAxisSize: MainAxisSize.min, // 이거 없으면 dialog가 엄청 길어요
+              crossAxisAlignment: CrossAxisAlignment.center, // 중앙 정렬
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    // hint 설정
+                      hintText: " 프로젝트명",
+                      hintStyle: TextStyle(
+                        fontFamily: "NanumSquareNeo",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black.withOpacity(0.3),
+                      ),
+
+                      // 각종 색 변경
+                      focusColor: const Color(0xFFE2583E),
+                      fillColor: const Color(0xFFE2583E),
+                      prefixIconColor: const Color(0xFFE2583E),
+                      suffixIconColor: const Color(0xFFE2583E),
+                      hoverColor: const Color(0xFFE2583E),
+                      iconColor: const Color(0xFFE2583E),
+
+                      // hint와 underline 사이의 공간 제거
+                      contentPadding: const EdgeInsets.all(0),
+                      isDense: true,
+
+                      // Underline의 색을 변경합니다
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFE2583E)),
+                      )
+                  ),
+                  cursorColor: const Color(0xFFE2583E), // 깜빡이는 커서의 색 변경
+                  maxLength: 20, // 최대 20자
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '시작일',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 12,
+                        fontFamily: 'NanumSquareNeo',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '종료일',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 12,
+                        fontFamily: 'NanumSquareNeo',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  // 상세내용이 왼쪽 정렬
+                  children: [
+                    Text(
+                      '상세내용',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 12,
+                        fontFamily: 'NanumSquareNeo',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                ConstrainedBox(
+                    constraints:
+                        const BoxConstraints(minHeight: 76, minWidth: 260),
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                width: 1, color: Color(0xFFD9D9D9)),
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(border: InputBorder.none),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 28,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: Container(
+                    width: 185,
+                    height: 25.60,
+                    alignment: Alignment.center,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFE2583E),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: const Text(
+                      '확인',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontFamily: 'NanumGothic',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
