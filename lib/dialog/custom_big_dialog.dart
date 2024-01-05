381c07f31bb820007f3afb96c86d9377ce328ca5
Jungwuk <vojougae35@gmail.com>
2024-01-05 22:50:17 +0900

    refactor: CustomBigDialog

---
+++ lib/dialog/custom_big_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomBigDialog extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const CustomBigDialog({super.key, this.title = "", required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: const Color(0xFFFFFFFF),
+      surfaceTintColor: const Color(0xFFFFFFFF),
+      // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      //Dialog Main Title
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: Color(0xFF333333),
+          fontSize: 12,
+          fontFamily: 'NanumSquareNeo',
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      content: child,
+    );
+  }
+}
--- lib/view/widget/add_team_dialog.dart
+++ lib/view/widget/add_team_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_weteam/dialog/custom_big_dialog.dart';
 import 'package:get/get.dart';
 
 class AddTeamDialog extends StatefulWidget {
@@ -16,135 +17,121 @@ class _AddTeamDialog extends State<AddTeamDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: const Color(0xFFFFFFFF),
-      surfaceTintColor: const Color(0xFFFFFFFF),
-      // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      //Dialog Main Title
-      title: const Text(
-        '팀플 추가',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Color(0xFF333333),
-          fontSize: 12,
-          fontFamily: 'NanumSquareNeo',
-          fontWeight: FontWeight.w800,
+    return CustomBigDialog(title: '팀플 추가', child: _body());
+  }
+
+  Widget _body() {
+    return Column(
+      mainAxisSize: MainAxisSize.min, // 이거 없으면 dialog가 엄청 길어요
+      crossAxisAlignment: CrossAxisAlignment.center, // 중앙 정렬
+      children: <Widget>[
+        _projectNameInputTextField(),
+        const SizedBox(
+          height: 12,
         ),
-      ),
-      //
-      content: Column(
-        mainAxisSize: MainAxisSize.min, // 이거 없으면 dialog가 엄청 길어요
-        crossAxisAlignment: CrossAxisAlignment.center, // 중앙 정렬
-        children: <Widget>[
-          _projectNameInputTextField(),
-          const SizedBox(
-            height: 12,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '시작일',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 12,
-                  fontFamily: 'NanumSquareNeo',
-                  fontWeight: FontWeight.w700,
-                ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '시작일',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 12,
+                fontFamily: 'NanumSquareNeo',
+                fontWeight: FontWeight.w700,
               ),
-              _dateText()
-            ],
-          ),
-          const SizedBox(
-            height: 13,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '종료일',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 12,
-                  fontFamily: 'NanumSquareNeo',
-                  fontWeight: FontWeight.w700,
-                ),
+            ),
+            _dateText()
+          ],
+        ),
+        const SizedBox(
+          height: 13,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '종료일',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 12,
+                fontFamily: 'NanumSquareNeo',
+                fontWeight: FontWeight.w700,
               ),
-              _dateText()
-            ],
-          ),
-          const SizedBox(
-            height: 13,
-          ),
-          Row(
-            // 상세내용이 왼쪽 정렬
-            children: [
-              Text(
-                '상세내용',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 12,
-                  fontFamily: 'NanumSquareNeo',
-                  fontWeight: FontWeight.w700,
-                ),
+            ),
+            _dateText()
+          ],
+        ),
+        const SizedBox(
+          height: 13,
+        ),
+        Row(
+          // 상세내용이 왼쪽 정렬
+          children: [
+            Text(
+              '상세내용',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 12,
+                fontFamily: 'NanumSquareNeo',
+                fontWeight: FontWeight.w700,
               ),
-            ],
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          ConstrainedBox(
-              constraints: const BoxConstraints(minHeight: 76, minWidth: 260),
-              child: Container(
-                padding: const EdgeInsets.all(5),
-                decoration: ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                      side:
-                          const BorderSide(width: 1, color: Color(0xFFD9D9D9)),
-                      borderRadius: BorderRadius.circular(8)),
-                ),
-                child: const TextField(
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    // 여백 제거
-                    contentPadding: EdgeInsets.all(0),
-                    isDense: true,
-                  ),
-                  cursorColor: Color(0xFFE2583E), // 깜빡이는 커서의 색 변경
-                  style: TextStyle(fontSize: 13),
-                ),
-              )),
-          const SizedBox(
-            height: 28,
-          ),
-          GestureDetector(
-            onTap: () {
-              Get.back();
-            },
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        ConstrainedBox(
+            constraints: const BoxConstraints(minHeight: 76, minWidth: 260),
             child: Container(
-              width: 185,
-              height: 25.60,
-              alignment: Alignment.center,
+              padding: const EdgeInsets.all(5),
               decoration: ShapeDecoration(
-                color: const Color(0xFFE2583E),
                 shape: RoundedRectangleBorder(
+                    side:
+                    const BorderSide(width: 1, color: Color(0xFFD9D9D9)),
                     borderRadius: BorderRadius.circular(8)),
               ),
-              child: const Text(
-                '확인',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                  fontFamily: 'NanumGothic',
-                  fontWeight: FontWeight.w700,
+              child: const TextField(
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  // 여백 제거
+                  contentPadding: EdgeInsets.all(0),
+                  isDense: true,
                 ),
+                cursorColor: Color(0xFFE2583E), // 깜빡이는 커서의 색 변경
+                style: TextStyle(fontSize: 13),
+              ),
+            )),
+        const SizedBox(
+          height: 28,
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          child: Container(
+            width: 185,
+            height: 25.60,
+            alignment: Alignment.center,
+            decoration: ShapeDecoration(
+              color: const Color(0xFFE2583E),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+            child: const Text(
+              '확인',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+                fontFamily: 'NanumGothic',
+                fontWeight: FontWeight.w700,
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
