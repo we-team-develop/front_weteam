11258b7982ddf1b35914812ab273ddef7c81ec63
Jungwuk Ryu <vojougae35@gmail.com>
2024-01-18 21:30:26 +0900

    feat: TeamPlayController를 MainBindings에 추가

--- lib/binding/main_bindings.dart
+++ lib/binding/main_bindings.dart
@@ -2,6 +2,7 @@ import 'package:front_weteam/controller/bottom_nav_controller.dart';
 import 'package:front_weteam/controller/home_controller.dart';
 import 'package:front_weteam/controller/my_controller.dart';
 import 'package:front_weteam/controller/profile_controller.dart';
+import 'package:front_weteam/controller/tp_controller.dart';
 import 'package:front_weteam/service/api_service.dart';
 import 'package:front_weteam/service/auth_service.dart';
 import 'package:front_weteam/util/provider/weteam_auth_provider.dart';
@@ -15,6 +16,7 @@ class MainBindings implements Bindings {
     Get.put(HomeController());
     Get.put(MyController());
     Get.put(ProfileController());
+    Get.put(TeamPlayController());
 
     Get.put(AuthService());
     Get.put(ApiService());
