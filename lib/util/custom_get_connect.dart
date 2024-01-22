cc3ead0241e9dd1ed34f038d74059d057aedff34
Jungwuk Ryu <vojougae35@gmail.com>
2024-01-22 19:59:52 +0900

    feat: FirebaseAuth 토큰 만료 방지

--- lib/util/custom_get_connect.dart
+++ lib/util/custom_get_connect.dart
@@ -1,49 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-import '../service/auth_service.dart';
-
 class CustomGetConnect extends GetConnect {
-  @override
-  Future<Response<T>> get<T>(
-      String url, {
-        Map<String, String>? headers,
-        String? contentType,
-        Map<String, dynamic>? query,
-        Decoder<T>? decoder,
-      }) {
-
+  Future<Map<String, String>?> putTokenToHeaders(
+      Map<String, String>? headers) async {
     headers ??= {};
     if (!headers.containsKey('Authorization')) {
-      headers['Authorization'] = 'Bearer ${Get.find<AuthService>().token}';
+      headers['Authorization'] =
+          'Bearer ${await FirebaseAuth.instance.currentUser?.getIdToken()}';
     }
 
-    return super.get<T>(
+    return headers;
+  }
+
+  @override
+  Future<Response<T>> get<T>(
+    String url, {
+    Map<String, String>? headers,
+    String? contentType,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+  }) async {
+    headers = await putTokenToHeaders(headers);
+
+    return await super.get<T>(
       url,
       headers: headers,
       contentType: contentType,
       query: query,
       decoder: decoder,
-
     );
   }
 
   @override
   Future<Response<T>> post<T>(
-      String? url,
-      dynamic body, {
-        String? contentType,
-        Map<String, String>? headers,
-        Map<String, dynamic>? query,
-        Decoder<T>? decoder,
-        Progress? uploadProgress,
-      }) {
+    String? url,
+    dynamic body, {
+    String? contentType,
+    Map<String, String>? headers,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+    Progress? uploadProgress,
+  }) async {
+    headers = await putTokenToHeaders(headers);
 
-    headers ??= {};
-    if (!headers.containsKey('Authorization')) {
-      headers['Authorization'] = 'Bearer ${Get.find<AuthService>().token}';
-    }
-
-    return super.post<T>(
+    return await super.post<T>(
       url,
       body,
       headers: headers,
@@ -56,19 +57,15 @@ class CustomGetConnect extends GetConnect {
 
   @override
   Future<Response<T>> delete<T>(
-      String url, {
-        Map<String, String>? headers,
-        String? contentType,
-        Map<String, dynamic>? query,
-        Decoder<T>? decoder,
-      }) {
+    String url, {
+    Map<String, String>? headers,
+    String? contentType,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+  }) async {
+    headers = await putTokenToHeaders(headers);
 
-    headers ??= {};
-    if (!headers.containsKey('Authorization')) {
-      headers['Authorization'] = 'Bearer ${Get.find<AuthService>().token}';
-    }
-
-    return super.delete(
+    return await super.delete(
       url,
       headers: headers,
       contentType: contentType,
