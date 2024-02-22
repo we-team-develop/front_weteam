c0ff59311b96410618516c315f8d1294a9ba42d7
Obebe <shkim000905@naver.com>
2024-02-22 21:24:40 +0900

    color update

--- lib/data/color_data.dart
+++ lib/data/color_data.dart
@@ -27,7 +27,11 @@ class AppColors {
   // Sub_Blue
   static const Color Blue_01 = Color(0xFF87B3DB);
   static const Color Blue_02 = Color(0xFF94A4F9);
-  static const Color Blue_03 = Color(0x9989B2E7);
+  static const Color Blue_03 = Color(0xFF89B2E7);
+  static const Color Blue_04 = Color(0xFFD6EBFA);
+  static const Color Blue_05 = Color(0xFFAED6F5);
+  static const Color Blue_06 = Color(0xFF85C1F1);
+  static const Color Blue_07 = Color(0xFF5DADEC);
   // Sub_Purple
   static const Color Purple = Color(0xFF9C879E);
   // Sub_Green
