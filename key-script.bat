95d903626eaeddb486d68d2682a3a7ec78c2815e
Jungwuk Ryu <vojougae35@gmail.com>
2024-02-22 17:50:21 +0900

    chore: key-script.bat 인코딩 ANSI로 변경

--- key-script.bat
+++ key-script.bat
@@ -1,13 +1,13 @@
 @echo off
 echo .
-echo ====== 서브모듈 추가 중... (git submodule add) ======
+echo ====== ������ �߰� ��... (git submodule add) ======
 git submodule add -f https://github.com/we-team-develop/front_weteam_submodule front_weteam_submodule
 echo .
-echo ====== 서브모듈 초기화 중... (git submodule init) ======
+echo ====== ������ �ʱ�ȭ ��... (git submodule init) ======
 git submodule init
 echo .
-echo ====== 서브 모듈 최신화 중... (git submodule update) ======
+echo ====== ���� ��� �ֽ�ȭ ��... (git submodule update) ======
 git submodule update --remote
 echo .
-echo ====== 파일을 적용하는 중... (robocopy) ======
+echo ====== ������ �����ϴ� ��... (robocopy) ======
 robocopy .\front_weteam_submodule\ .\ /E /IS
