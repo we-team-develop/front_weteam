ff66269bb428de9cf604c0e5d56ed70c8b368993
jungwuk <vojougae35@gmail.com>
2024-05-07 00:37:31 +0900

    design: 다른 유저 정보 조회시 상하단 검정 여백 제거

--- lib/view/my/mypage.dart
+++ lib/view/my/mypage.dart
@@ -87,10 +87,11 @@ class _UserInfoPageState extends State<UserInfoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: _body(),
-    ));
+    return Scaffold(
+      body: SafeArea(
+        child: _body(),
+      ),
+    );
   }
 
   Widget _body() {
